aa361fb8137bb09e0843cce22c320eb5ee4564c9
Bansee Abdelnasser <banceinasser36@gmail.com>
2023-12-12 02:31:33 +0200

    done Registration design and controller

--- src/main/java/coworking_space/coworks/RegisterController.java
+++ src/main/java/coworking_space/coworks/RegisterController.java
@@ -5,19 +5,49 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class RegisterController {
+
+    @FXML
+    private ToggleGroup Visitor_Type;
+
+    @FXML
+    private PasswordField confirmPassword;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    public PasswordField password;
+
+    @FXML
+    private TextField phoneNumber;
 
     @FXML
     private Button registerLogin;
 
     @FXML
+    private Button registerLogin1;
+
+    @FXML
     private AnchorPane registerScreen;
+
+    @FXML
+    public TextField userName;
 
     @FXML
     void changeScreenToLoginScreen_1(MouseEvent event) throws IOException
@@ -35,5 +65,23 @@ public class RegisterController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    void saveInfoAndRedirect(MouseEvent event) throws IOException, NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+       /* String textFromuserName = userName.getText();
+        String textFromEmail = email.getText();
+        String textFromPhoneNumber = phoneNumber.getText();
+        String textFromPassword = password.getText();
+        String textFromConfirmPassword = confirmPassword.getText();
+
+*/
+      //  Parent root = FXMLLoader.load(getClass().getResource("registerScreen.fxml"));
+        //System.out.println("Text from user: " + userName.getText());
+        System.out.println("Text from password: " + password.getText());
+
+    }
+
+
 
 }
+
+
