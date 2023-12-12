17ed1aa89bb3601443341f641ceff8759baf2edd
Haneen <haneenhany1205@gmail.com>
2023-12-12 03:53:04 +0200

    validation for registration controller

--- src/main/java/coworking_space/coworks/RegisterController.java
+++ src/main/java/coworking_space/coworks/RegisterController.java
@@ -2,14 +2,15 @@ package coworking_space.coworks;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import javax.crypto.BadPaddingException;
@@ -19,6 +20,8 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
 
 public class RegisterController {
 
@@ -65,9 +68,24 @@ public class RegisterController {
         stage.setScene(scene);
         stage.show();
     }
+
     @FXML
-    void saveInfoAndRedirect(MouseEvent event) throws IOException, NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-       /* String textFromuserName = userName.getText();
+    void initialize(){
+        password.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {validatePassword(password.getText(),password.getText());}});
+        confirmPassword.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {validatePassword(password.getText(),confirmPassword.getText());}});
+        phoneNumber.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {validatePhoneNumber(phoneNumber.getText());}});
+        userName.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {validateUsername(userName.getText());}});
+        email.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {validateEmail(email.getText());}});
+    }
+    @FXML
+    void saveInfoAndRedirect(MouseEvent Event) throws IOException, NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+
+        /* String textFromuserName = userName.getText();
         String textFromEmail = email.getText();
         String textFromPhoneNumber = phoneNumber.getText();
         String textFromPassword = password.getText();
@@ -76,9 +94,139 @@ public class RegisterController {
 */
       //  Parent root = FXMLLoader.load(getClass().getResource("registerScreen.fxml"));
         //System.out.println("Text from user: " + userName.getText());
-        System.out.println("Text from password: " + password.getText());
+//        System.out.println("Text from password: " + password.getText());
 
     }
+    private boolean passwordRegex(String password) {
+        // Define the password validation regex
+        String passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{8,}$";
+
+        // Check if the password matches the regex
+        boolean isValid = Pattern.matches(passwordRegex, password);
+
+        // Display a pop-up with the validation result
+        return isValid;
+    }
+
+    private boolean phoneNumberRegex(String phoneNumber) {
+
+        String phoneNumberRegex = "^[0-9]{10}$";
+
+        // Check if the password matches the regex
+        boolean isValid = Pattern.matches(phoneNumberRegex,phoneNumber);
+
+        // Display a pop-up with the validation result
+        return isValid;
+    }
+
+    private boolean emailRegex(String email) {
+
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+
+        boolean isValid = Pattern.matches(emailRegex, email);
+
+        // Display a pop-up with the validation result
+        return isValid;
+    }
+    private boolean usernameRegex(String username) {
+
+        String usernameRegex = "^[a-zA-Z0-9_]{3,20}$";
+
+
+        boolean isValid = Pattern.matches(usernameRegex, username);
+
+        // Display a pop-up with the validation result
+        return isValid;
+    }
+
+    private void validatePassword(String password, String confirmPassword) {
+        boolean isValid = passwordRegex(password);
+        boolean equalsConfirmPass = password.equals(confirmPassword);
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.setTitle("Password Validation");
+        if(!isValid || !equalsConfirmPass) {
+            Label Message = new Label("Invalid password. Please try again");
+
+            Button closeButton = new Button("Close");
+            closeButton.setOnAction(event -> popupStage.close());
+
+            VBox layout = new VBox(10);
+            layout.setPadding(new Insets(10));
+            layout.getChildren().addAll(Message, closeButton);
+
+            Scene scene = new Scene(layout, 300, 100);
+            popupStage.setScene(scene);
+            popupStage.showAndWait();
+        }
+    }
+
+    private void validatePhoneNumber(String phoneNumber) {
+        boolean isValid = phoneNumberRegex(phoneNumber);
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.setTitle("Phone Number Validation");
+        if(!isValid) {
+            Label Message = new Label("Invalid phone number. Please try again");
+
+            Button closeButton = new Button("Close");
+            closeButton.setOnAction(event -> popupStage.close());
+
+            VBox layout = new VBox(10);
+            layout.setPadding(new Insets(10));
+            layout.getChildren().addAll(Message, closeButton);
+
+            Scene scene = new Scene(layout, 300, 100);
+            popupStage.setScene(scene);
+            popupStage.showAndWait();
+        }
+    }
+
+    private void validateEmail(String email) {
+        boolean isValid = emailRegex(email);
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.setTitle("Email Validation");
+        if(!isValid) {
+            Label Message = new Label("Invalid email. Please try again");
+
+            Button closeButton = new Button("Close");
+            closeButton.setOnAction(event -> popupStage.close());
+
+            VBox layout = new VBox(10);
+            layout.setPadding(new Insets(10));
+            layout.getChildren().addAll(Message, closeButton);
+
+            Scene scene = new Scene(layout, 300, 100);
+            popupStage.setScene(scene);
+            popupStage.showAndWait();
+        }
+    }
+
+    private void validateUsername(String username) {
+        boolean isValid = usernameRegex(username);
+        Stage popupStage = new Stage();
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.setTitle("Username Validation");
+        if(!isValid) {
+            Label Message = new Label("Invalid username. Please try again");
+
+            Button closeButton = new Button("Close");
+            closeButton.setOnAction(event -> popupStage.close());
+
+            VBox layout = new VBox(10);
+            layout.setPadding(new Insets(10));
+            layout.getChildren().addAll(Message, closeButton);
+
+            Scene scene = new Scene(layout, 300, 100);
+            popupStage.setScene(scene);
+            popupStage.showAndWait();
+        }
+    }
+
+
+
 
 
 
