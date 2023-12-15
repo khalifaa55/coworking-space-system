b15999a8ba65f82702e591fb8ce8cd782f576eda
Fagr <Fagr-Ashraf>
2023-12-15 16:24:42 +0200

    .

--- src/main/java/coworking_space/coworks/Coworks_Main.java
+++ src/main/java/coworking_space/coworks/Coworks_Main.java
@@ -10,6 +10,27 @@ import java.util.ArrayList;
 
 public class Coworks_Main extends Application
 {
+
+    public static ArrayList<AbstractRoom> meetingRooms=new ArrayList<>();
+
+    public static ArrayList<Slot> Mslots = new ArrayList<>();
+    public static  ArrayList<FormalVisitor> formals= new ArrayList<>();
+    public static ArrayList<Slot> Mslots2 = new ArrayList<>();
+    public static   ArrayList<FormalVisitor> formals2= new ArrayList<>();
+    public static  ArrayList<Slot> Mslots3 = new ArrayList<>();
+    public static  ArrayList<FormalVisitor> formals3= new ArrayList<>();
+
+
+
+    public static ArrayList<AbstractRoom> teachingRooms=new ArrayList<>();
+
+    public static ArrayList<Slot> tslots = new ArrayList<>();
+    public static  ArrayList<InstructorVisitor> insts= new ArrayList<>();
+    public static ArrayList<Slot> tslots2 = new ArrayList<>();
+    public static   ArrayList<InstructorVisitor> insts2= new ArrayList<>();
+    public static  ArrayList<Slot> tslots3 = new ArrayList<>();
+    public static  ArrayList<InstructorVisitor> insts3= new ArrayList<>();
+
     @Override
     public void start(Stage stage) throws IOException
     {
@@ -32,12 +53,12 @@ public class Coworks_Main extends Application
 
 //        final String WRITE_MEETING_ROOMS_PATH = "src/main/resources/meetingRooms.json";
 //        final String READ_MEETING_ROOMS_PATH = "meetingRooms.json";
-         ArrayList<AbstractRoom> meetingRooms=new ArrayList<>();
 
-        ArrayList<Slot> slots = new ArrayList<>();
-        ArrayList<FormalVisitor> insts= new ArrayList<>();
-        FormalVisitor inst= new FormalVisitor("h",12);
-        FormalVisitor inst2 = new FormalVisitor("R",14);
+
+
+
+        FormalVisitor formal= new FormalVisitor("h",12);
+        FormalVisitor formal0 = new FormalVisitor("R",14);
 
         FormalVisitor formal1= new FormalVisitor("y",123);
         FormalVisitor formal2 = new FormalVisitor("z",143);
@@ -45,11 +66,7 @@ public class Coworks_Main extends Application
         FormalVisitor formal3= new FormalVisitor("G",122);
         FormalVisitor formal4 = new FormalVisitor("D",142);
 
-        ArrayList<Slot> slots2 = new ArrayList<>();
-        ArrayList<FormalVisitor> insts2= new ArrayList<>();
 
-        ArrayList<Slot> slots3 = new ArrayList<>();
-        ArrayList<FormalVisitor> insts3= new ArrayList<>();
 
 //        try {
 //            meetingRooms  = Json.readRoomsFromFile(READ_MEETING_ROOMS_PATH);
@@ -69,29 +86,76 @@ public class Coworks_Main extends Application
 
         // slot2.createReservation(inst);
         //slot1.createReservation(inst);
-        insts.add(inst);
-        insts.add(inst2);
-        slots.add(slot1);
-        slots.add(slot2);
-        MeetingRoom meeting_room= new MeetingRoom("A",123,slots,insts);
+        formals.add(formal);
+        formals.add(formal0);
+        Mslots.add(slot1);
+        Mslots.add(slot2);
+        MeetingRoom meeting_room= new MeetingRoom("A",123,Mslots,formals);
         meetingRooms.add(meeting_room);
 
-        MeetingRoom meeting_room2= new MeetingRoom("B",124,slots2,insts2);
-        MeetingRoom meeting_room3= new MeetingRoom("C",125,slots3,insts3);
+        MeetingRoom meeting_room2= new MeetingRoom("B",124,Mslots2,formals2);
+        MeetingRoom meeting_room3= new MeetingRoom("C",125,Mslots3,formals3);
         meetingRooms.add(meeting_room2);
         meetingRooms.add(meeting_room3);
 
         //slot1.createReservation(inst);
-        insts2.add(formal1);
-        insts3.add(formal2);
-        slots2.add(slot3);
-        slots2.add(slot4);
+        formals2.add(formal1);
+        formals2.add(formal2);
+        Mslots2.add(slot3);
+        Mslots2.add(slot4);
 
         //slot1.createReservation(inst);
-        insts3.add(formal3);
-        insts3.add(formal4);
-        slots3.add(slot5);
-        slots3.add(slot6);
+        formals3.add(formal3);
+        formals3.add(formal4);
+        Mslots3.add(slot5);
+        Mslots3.add(slot6);
+
+
+
+
+
+
+
+//        InstructorVisitor inst1= new InstructorVisitor("h",12);
+//        InstructorVisitor inst2 = new InstructorVisitor("R",14);
+//
+//        InstructorVisitor inst3= new InstructorVisitor("y",123);
+//        InstructorVisitor inst4 = new InstructorVisitor("z",143);
+//
+//        InstructorVisitor inst5= new InstructorVisitor("G",122);
+//        InstructorVisitor inst6 = new InstructorVisitor("D",142);
+
+
+        Slot slot7 = new Slot("8/11","9/11", 100.00);
+        Slot slot8 = new Slot("11/11","10/10", 300);
+
+
+        Slot slot9 = new Slot("8/11","9/11", 100.00);
+        Slot slot10 = new Slot("11/11","10/10", 300);
+
+        Slot slot11 = new Slot("8/11","9/11", 100.00);
+        Slot slot12 = new Slot("11/11","10/10", 300);
+
+
+
+        tslots.add(slot7);
+        tslots.add(slot8);
+        TeachingRoom teaching_room= new TeachingRoom("A",123,tslots,insts);
+        meetingRooms.add(meeting_room);
+
+        TeachingRoom teaching_room2= new TeachingRoom("B",124,tslots2,insts2);
+        TeachingRoom teaching_room3= new TeachingRoom("C",125,tslots3,insts3);
+        meetingRooms.add(meeting_room2);
+        meetingRooms.add(meeting_room3);
+
+        //slot1.createReservation(inst);
+
+        tslots2.add(slot9);
+        tslots2.add(slot10);
+
+        tslots3.add(slot11);
+        tslots3.add(slot12);
+
 
 
 
--- src/main/java/coworking_space/coworks/VisitorController.java
+++ src/main/java/coworking_space/coworks/VisitorController.java
@@ -23,6 +23,15 @@ public class VisitorController implements Initializable {
     // ObservableList to hold the slot strings for the ChoiceBox
     private ObservableList<String> observableSlots = FXCollections.observableArrayList();
 
+    private ArrayList<AbstractRoom> meetingRooms = Coworks_Main.meetingRooms;
+    private ArrayList<Slot> slots  = Coworks_Main.Mslots;
+    private ArrayList<Slot> slots2  = Coworks_Main.Mslots2;
+    private ArrayList<Slot> slots3  = Coworks_Main.Mslots3;
+    private ArrayList<FormalVisitor> insts=Coworks_Main.formals;
+    private ArrayList<FormalVisitor> insts2=Coworks_Main.formals2;
+    private ArrayList<FormalVisitor> insts3=Coworks_Main.formals3;
+
+
 
     @FXML
     private RadioButton Room1;
@@ -32,69 +41,67 @@ public class VisitorController implements Initializable {
 
     @FXML
     private RadioButton Room3;
+    private void updatechoiceBox() {
+        // Clear the observable list
+        observableSlots.clear();
+
+        // Populate the observable list with slot information
+        for (Slot slot : availableSlots) {
+            // Customize this to format the slot information as needed
+            String slotInfo = slot.getStartTime() + " - " + slot.getEndTime();
+            observableSlots.add(slotInfo);
+        }
+
+        // Set the updated observable list as the items for the ChoiceBox
+        choiceBox.setItems(observableSlots);
+    }
+    private void retrieveAvailableSlots(int i) {
+        availableSlots.clear();
+
+        if(RegisterController.formaltype){
+            AbstractRoom meeting_room = meetingRooms.get(i);
+            availableSlots = meeting_room.getAvailableSlots();
+
+
+        } else if (RegisterController.instructortype) {
+
+        }else{
+
+        }
+
+
+        updatechoiceBox();
+    }
 
 
     @FXML
     void getRoom1(MouseEvent event) {
-//        private void updatechoiceBox() {
-//            // Clear the observable list
-//            observableSlots.clear();
-//
-//            // Populate the observable list with slot information
-//            for (Slot slot : availableSlots) {
-//                // Customize this to format the slot information as needed
-//                String slotInfo = slot.getStartTime() + " - " + slot.getEndTime();
-//                observableSlots.add(slotInfo);
-//            }
-//
-//            // Set the updated observable list as the items for the ChoiceBox
-//            choiceBox.setItems(observableSlots);
-//        }
-//
-//        // Method to retrieve available slots and update the ChoiceBox
-//        private void retrieveAvailableSlots() {
-//            if(RegisterController.formaltype){
-//
-//            } else if (RegisterController.instructortype) {
-//
-//            }else{
-//
-//            }
-//
-//
-//            updatechoiceBox();
-//        }
 
-
-
-
+        retrieveAvailableSlots(0);
     }
 
 
     void getRoom2(MouseEvent event) {
-        choiceBox.getItems().clear();
-
-
+        retrieveAvailableSlots(1);
 
     }
 
     @FXML
     void getRoom3(MouseEvent event) {
-        choiceBox.getItems().clear();
 
-
-
+        retrieveAvailableSlots(2);
     }
 
     @FXML
     void getselectedslot(MouseEvent event) {
+        String selectedSlot = choiceBox.getSelectionModel().getSelectedItem();
+        // Use the selected slot as needed
+        System.out.println("Selected Slot: " + selectedSlot);
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
-
-
+        choiceBox.setItems(observableSlots);
     }
 }
